dd1249408e540062837682e696ba4536b6507321
AvianExrpess <bronevasya@gmail.com>
2021-05-09 14:47:09 +0700

    adding files

--- task.js
+++ task.js
@@ -1,3 +1,5 @@
+//Сторонняя библиотека
+//Я не знаю, почему с ней установилась ещё целая куча сторонней штуки, я пытался её удалить чтобы привести проект в порядочный вид, но он сильно капризничает, поэтому пока так
 const yargs = require('yargs/yargs');
 const { hideBin } = require('yargs/helpers');
 const argv = yargs(hideBin(process.argv)).argv;
@@ -5,6 +7,7 @@ const argv = yargs(hideBin(process.argv)).argv;
 
 class hamster 
 {
+    //Конструктор стандартный, ничего сложного
     constructor(name,sound, wheelspeed,food)
     {
         this.name=name;
@@ -13,34 +16,37 @@ class hamster
         this.food=food;
     }
     whatfood()
-    {   console.log(`${this.name} хомяк питается следующим: ${this.food}`);
-        return `${this.name} хомяк питается следующим: ${this.food}`;
+    {   
+        //Тут должен быть return, но в консоль легче выводить так
+        console.log(`${this.name} хомяк питается следующим: ${this.food}`);
     }
     howloud()
     {
         console.log(`${this.name} хомяк кричит ${this.sound}`);
-        return `${this.name} хомяк кричит ${this.sound}`;
     }
     howfast()
     {
         console.log(`${this.name} хомяк крутит колесо со скоростью ${this.wheelspeed}`);
-        return `${this.name} хомяк крутит колесо со скоростью ${this.wheelspeed}`;
     }
 
 
 
 }
+    //Спавним хомяков
     let common=new hamster("Обычный", "средне",10+" оборотов в минуту",["Злаки","Пшено","Кукуруза","Семечки"]);
     let junharian=new hamster("Джунгарский", "слишком громко, лучше не слышать",25+" оборотов в минуту",["Арбуз","Фасоль","Горох","Семечки"]);
     let wildforest=new hamster("Дикий лесной", "тихо",15+" оборотов в минуту",["Кора","Насекомые","Кукуруза","Птицы"]);
     let angorian=new hamster("Ангорский", "громко",0+" оборотов в минуту... он вообше жив?",["KFC крылышки","Макдональдс","Кока-кола","Чипсы"]);
     let rubber=new hamster("Резиновый с пищалкой", "средне",5+" оборотов в минуту",["Клей момент","Вода дистиллированная"]);
+
+    //Если вдруг забудем аргументы, то он хоть что-то выдаст
     try
     {
         eval(argv.r+"."+argv.n+"()");
     }
     catch(e)
     {
+        //А это если мы хотим просто получить значение переменной, некрасиво, но да ладно
             try
         {
             console.log(eval(argv.r+"."+argv.n));
